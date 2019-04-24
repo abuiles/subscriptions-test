00cd3db49240a95b5c64a1e5bf699e28d3aad8a2
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-24 16:20:14 -0500

    Subscribe to event.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
+import { ApolloLink } from 'apollo-link';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ActionCable from 'actioncable';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
+import gql from "graphql-tag";
 import './App.css';
+
+const cable = ActionCable.createConsumer('http://localhost:3000/subscriptions')
+
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const hasSubscriptionOperation = ({ query: { definitions } }) => {
+  return definitions.some(
+    ({ kind, operation }) => kind === 'OperationDefinition' && operation === 'subscription'
+  )
+}
+
+const link = ApolloLink.split(
+  hasSubscriptionOperation,
+  new ActionCableLink({cable}),
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
+
+let o = client
+  .subscribe({
+    query: gql`
+      subscription {
+        deliveryCreated {
+          id
+        }
+      }
+    `
+  });
+
+o.subscribe({
+  next(data) {
+    console.log('data! --- ', data)
+  },
+  error(value) {
+    console.log('erro! --- ', value)
+  }
+});
+
 
 function App() {
   return (
